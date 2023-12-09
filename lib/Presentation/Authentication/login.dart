005facc05a029f49d0442dad3f8b994b80fbcbdc
sinan <ruzcho.sinan@gmail.com>
2023-12-09 18:53:57 +0500

    login page ui theme changed

--- lib/Presentation/Authentication/login.dart
+++ lib/Presentation/Authentication/login.dart
@@ -3,11 +3,9 @@
 import 'package:desiree/Presentation/Authentication/register_screen.dart';
 import 'package:desiree/Presentation/TextConfig/text_config.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 
 import '../../Controllers/authentication_controller.dart';
-import '../Components/background.dart';
 import '../Components/buttons.dart';
 import '../Components/text_inputs.dart';
 
@@ -24,95 +22,119 @@ class _LoginState extends State<Login> {
 
   @override
   Widget build(BuildContext context) {
-    return BackgroundWidget(
-      header: 'Login',
-      widget: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 38),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+    double pixH = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+            padding:  EdgeInsets.only(top: pixH * 0.08,left: 38,right: 38),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
 
-          children: [
-            AppText.label('Welcome Back',color: Colors.black),
-           SizedBox(height: 10,),
-            AppText.small('Enter your credentials to continue.',color: Color(0xFF727272)),
-            SizedBox(height: 48,),
+                children: [
+                  Center(
+                    child: AppText.header('Talky'),
+                  ),
+                  SizedBox(height: pixH * 0.06,),
+                  AppText.screenHeader('Welcome Back',color: Colors.black),
+                 SizedBox(height: 10,),
+                  AppText.micro('Enter your credentials to continue.',color: Color(0xFF727272)),
+                  SizedBox(height: 48,),
 
-            Obx(() => MainInput(text: 'Email',
-              onChanged: (val){
-              _authController.setEmail(val);
-              _authController.validateEmail();
-            },
-              errorMessage: _authController.emailError.value,
-            ),),
-            SizedBox(height: 18,),
-            Obx(() => MainInput(
-              text: 'Password',onChanged: (val){
-              _authController.setPassword(val);
-            },
-              errorMessage: _authController.passwordError.value,
-            ),),
-            SizedBox(height: 10,),
+                  Obx(() => MainInput(text: 'Email',
+                    onChanged: (val){
+                    _authController.setEmail(val);
+                    _authController.validateEmail();
+                  },
+                    errorMessage: _authController.emailError.value,
+                  ),),
+                  SizedBox(height: 18,),
+                  Obx(() => MainInput(
+                    text: 'Password',onChanged: (val){
+                    _authController.setPassword(val);
+                  },
+                    errorMessage: _authController.passwordError.value,
+                  ),),
+                  SizedBox(height: 10,),
 
-            AppText.micro('Forgot username/password?'),
+                  AppText.micro('Forgot username/password?'),
 
-            SizedBox(height: 48,),
+                  SizedBox(height: 48,),
 
-            PrimaryButton(text: 'Login',
-              onTap: () {
-              _authController.validateEmailPasswordEmpty();
-            },),
+                  PrimaryButton(text: 'Login',
+                    onTap: () {
+                    _authController.validateEmailPasswordEmpty();
+                  },),
 
-            SizedBox(height: 18,),
+                  SizedBox(height: 18,),
 
-            Center(
-              child: AppText.micro('- or login via -',color:Color(0xFF727272),),
+                  Center(
+                    child: AppText.micro('- or login via -',color:Color(0xFF727272),),
+                  ),
+                  SizedBox(height: 18,),
+
+
+                 Row(
+                   mainAxisAlignment: MainAxisAlignment.center,
+                   children: [
+                     SocialButtons("Google",path: 'assets/svgs/google.svg',
+                     onPress: () async {
+
+                      await _authController.signInWithGoogle().then((value){
+                        if(value){
+                          Get.snackbar("Auth Message",
+                              "Login Successful");
+                        }
+                        else{
+                          Get.snackbar("Auth Message",
+                              "User does not exist");
+
+                        }
+                      });
+
+                     }
+                     ),
+                     SizedBox(width: 30,),
+                     SocialButtons("Facebook",path: 'assets/svgs/twitter.svg'),
+                     SizedBox(width: 30,),
+                     SocialButtons("Apple",path: 'assets/svgs/logos_apple.svg'),
+                     SizedBox(width: 30,),
+
+                   ],
+                 ),
+
+                  SizedBox(height: 40,),
+
+                  Center(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        AppText.micro('Don\'t have an account'),
+                        TextButton(onPressed: (){
+                          Get.to(const RegisterScreen());
+                        },
+                            child: AppText.micro('Register',color: Color(0xff377DFF)))
+
+                      ],
+                    ),
+                  )
+
+                  // bottomBox("Don’t have account?","register",(){
+                  //   Get.to(const RegisterScreen());
+                  // })
+
+
+
+
+
+
+                ],
+              ),
             ),
-            SizedBox(height: 18,),
-
-
-           Row(
-             mainAxisAlignment: MainAxisAlignment.center,
-             children: [
-               SocialButtons("Google",path: 'assets/svgs/google.svg',
-               onPress: () async {
-
-                await _authController.signInWithGoogle().then((value){
-                  if(value){
-                    Get.snackbar("Auth Message",
-                        "Login Successful");
-                  }
-                  else{
-                    Get.snackbar("Auth Message",
-                        "User does not exist");
-
-                  }
-                });
-
-               }
-               ),
-               SizedBox(width: 30,),
-               SocialButtons("Facebook",path: 'assets/svgs/twitter.svg'),
-               SizedBox(width: 30,),
-               SocialButtons("Apple",path: 'assets/svgs/logos_apple.svg'),
-               SizedBox(width: 30,),
-
-             ],
-           ),
-
-            SizedBox(height: 30,),
-
-            bottomBox("Don’t have account?","register",(){
-              Get.to(const RegisterScreen());
-            })
-
-
-
-
-
-
-          ],
-        ),
-      ),);
+          ),
+      ),
+    );
   }
 
 
